587b85b5312198851fd3b54cda0f5efec4bf2924
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-21 15:55:55 -0400

    Get user list (only when there's a verified token in ls

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        console.log(window.localStorage.getItem("token"));
+        axios.get("http://localhost:8000/api/users/", {headers: { authorization : JSON.parse(window.localStorage.getItem("token")) }})
+        .then(res => {
+            console.log("Get users DATA", res.data);
+            setUsers(res.data);
+            console.log("USERS", users);
+        })
+        .catch(err => console.log("Get users ERROR", err))
+    }, [])
+
     return (
         <div className = "dashboard">
             <h2>Dashboard</h2>
+            {/* {users.map(user => (
+                <div key = {user.id} className = "user"> 
+                    <p>{user.username}</p>
+                </div>
+            ))} */}
         </div>
     )
 };
