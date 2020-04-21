dc18e0374b6c10a7985d3ac53b10805d1f788570
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-21 14:38:59 -0400

    Setup server

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router");
+const authenticator = require("../auth/authenticator.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/users", authenticator, usersRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
---
+++ auth/auth-router.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const rounds = process.env.HASH_ROUNDS || 8;
+
+  const hash = bcrypt.hashSync(user.password, rounds);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  // Search for the user using the username
+  Users.findBy({ username })
+    .then(user => {
+      // console.log("user", user[0]);
+      // If we find the user, then also check that the passwords match
+      if(user && bcrypt.compareSync(password, user[0].password)) {
+        // Produce a token
+        const token = generateToken(user);
+        // Send the token to the client
+        res.status(200).json({ message: "Welcome!", token });
+      } else {
+        res.status(401).json({ message: "You cannot pass!"})
+      }
+      
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    userId: user[0].id,
+    username:  user[0].username
+  };
+
+  const secret = process.env.JWT_SECRET || "blahblahblah";
+
+  const options = {
+    expiresIn: "1d"
+  }
+
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
